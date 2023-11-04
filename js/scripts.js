09edade15e2303e36ed9ceb2436bd6448a1a3c49
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-04 10:44:41 -0700

    Update & add new form to allow people to log in WIP.

--- js/scripts.js
+++ js/scripts.js
@@ -27,20 +27,22 @@ document.addEventListener("DOMContentLoaded", (e) => {
     e.preventDefault();
     const registrationForm = document.querySelector("form#registrationForm");
     const registrationMessage = document.querySelector("p#registrationMessage");
+    const logInLink = document.querySelector("a#logInLink");
 
+    logInLink.addEventListener("click", logIn);
     registrationForm.addEventListener("submit", (e) => {
         e.preventDefault();
-
         const firstName = document.querySelector("input#firstNameInput").value;
         const lastName = document.querySelector("input#lastNameInput").value;
         const email = document.querySelector("input#emailInput").value;
         const password = document.querySelector("input#pwdInput").value;
-
         let newUser = new Registration(firstName, lastName, email, password);
+
         if (!validEmail(email)) {
             registrationMessage.innerText = "Please enter a valid email address."
             return;
         }
+
         registeredUser.addUser(newUser);
         registrationMessage.innerText = "Registration successful for " + newUser.firstName + " " + newUser.lastName + " with email " + newUser.email;
     });
@@ -48,5 +50,11 @@ document.addEventListener("DOMContentLoaded", (e) => {
     function validEmail(email) {
         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         return emailRegex.test(email);
+    }
+
+    function logIn(e) {
+        e.preventDefault();
+        document.querySelector("form#registrationForm").classList.add("hidden");
+        document.querySelector("form#logInForm").classList.remove("hidden");
     }
 });
